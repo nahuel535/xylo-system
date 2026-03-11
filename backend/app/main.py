f3e3c919f4ff869fd347d182da8fa69b3fb3597a
Nahuel Fernandez <nahufc9@gmail.com>
2026-03-11 11:37:49 -0300

    ahora chi

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "xylo-system-aymiyqvys-sefes-projects.vercel.app",
+        "https://xylo-system-aymiyqvys-sefes-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
