fcd4d5b91e7c44d7798ba66d9bf7152cd5cb108f
Nahuel Fernandez <nahufc9@gmail.com>
2026-03-20 15:27:17 -0300

    fied

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -4,4 +4,5 @@ from app.models.user import User
 from app.models.product import Product
 from app.models.sale import Sale
 from app.models.sale_payment import SalePayment
-from app.models.exchange_rate_model import ExchangeRatefrom app.models.audit_log import AuditLog
+from app.models.exchange_rate_model import ExchangeRate
+from app.models.audit_log import AuditLog
